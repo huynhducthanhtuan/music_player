6ca90eaabe6cfeff0731bfaebc6101dfe6cf61a5
huynhdthanhtuan <tuan40655@gmail.com>
2021-09-02 13:01:19 +0700

    Prepare for JS handle

---
+++ app.js
@@ -0,0 +1,122 @@
+const songs = [
+    {
+        "name": "Âm Thầm Bên Em",
+        "author": "Sơn Tùng M-TP",
+        "image": "./assest/img/1.png",
+        "audio": "./assest/audio/1.mp3"
+    },
+    {
+        "name": "Anh Không Sao Đâu",
+        "author": "Chi Dân",
+        "image": "./assest/img/2.png",
+        "audio": "./assest/audio/2.mp3"
+    },
+    {
+        "name": "Bay Giữa Ngân Hà",
+        "author": "Nam Cường",
+        "image": "./assest/img/3.png",
+        "audio": "./assest/audio/3.mp3"
+    },
+    {
+        "name": "Câu Hẹn Câu Thề",
+        "author": "Đình Dũng",
+        "image": "./assest/img/4.png",
+        "audio": "./assest/audio/4.mp3"
+    },
+    {
+        "name": "Cơn Mưa Ngang Qua",
+        "author": "Sơn Tùng M-TP",
+        "image": "./assest/img/5.png",
+        "audio": "./assest/audio/5.mp3"
+    },
+    {
+        "name": "Đom Đóm",
+        "author": "Jack",
+        "image": "./assest/img/6.png",
+        "audio": "./assest/audio/6.mp3"
+    },
+    {
+        "name": "Dừng Thương",
+        "author": "DatKaa",
+        "image": "./assest/img/7.png",
+        "audio": "./assest/audio/7.mp3"
+    },
+    {
+        "name": "Em Của Ngày Hôm Qua",
+        "author": "Sơn Tùng M-TP",
+        "image": "./assest/img/8.png",
+        "audio": "./assest/audio/8.mp3"
+    },
+    {
+        "name": "Em Luôn Ở Trong Tâm Trí Anh",
+        "author": "The Men",
+        "image": "./assest/img/9.png",
+        "audio": "./assest/audio/9.mp3"
+    },
+    {
+        "name": "Già Cùng Nhau Là Được",
+        "author": "Tùng TeA ft. PC",
+        "image": "./assest/img/10.png",
+        "audio": "./assest/audio/10.mp3"
+    },
+    {
+        "name": "Im Lặng",
+        "author": "LK",
+        "image": "./assest/img/11.png",
+        "audio": "./assest/audio/11.mp3"
+    },
+    {
+        "name": "L.I.P",
+        "author": "LK",
+        "image": "./assest/img/12.png",
+        "audio": "./assest/audio/12.mp3"
+    },
+    {
+        "name": "Mây Lang Thang",
+        "author": "Tùng TeA ft. PC",
+        "image": "./assest/img/13.png",
+        "audio": "./assest/audio/13.mp3"
+    },
+    {
+        "name": "Nắng Ấm Xa Dần",
+        "author": "Sơn Tùng M-TP",
+        "image": "./assest/img/14.png",
+        "audio": "./assest/audio/14.mp3"
+    },
+    {
+        "name": "Người Có Thương",
+        "author": "DatKaa",
+        "image": "./assest/img/15.png",
+        "audio": "./assest/audio/15.mp3"
+    },
+    {
+        "name": "Nơi Đâu Tìm Thấy Em",
+        "author": "Chu Bin",
+        "image": "./assest/img/16.png",
+        "audio": "./assest/audio/16.mp3"
+    },
+    {
+        "name": "Sài Gòn Hôm Nay Mưa",
+        "author": "JSOL ft. Hoàng Duyên",
+        "image": "./assest/img/17.png",
+        "audio": "./assest/audio/17.mp3"
+    },
+    {
+        "name": "Tát Nước Đầu Đình",
+        "author": "Lynk Lee ft. Binz",
+        "image": "./assest/img/18.png",
+        "audio": "./assest/audio/18.mp3"
+    },
+    {
+        "name": "Tau Thích Mi",
+        "author": "Lil Pig",
+        "image": "./assest/img/19.png",
+        "audio": "./assest/audio/19.mp3"
+    },
+    {
+        "name": "Về Bên Anh",
+        "author": "Jack",
+        "image": "./assest/img/20.png",
+        "audio": "./assest/audio/20.mp3"
+    }
+];
